8884de64eeddc727d5182acf272d33643fbddb88
Aleksandr Frolov <fr.28.09.07@gmail.com>
2025-08-07 13:40:17 +0200

    fixed new logo in figma and add it to website

--- js/logo.js
+++
@@ -1,9 +0,0 @@
-const logo = document.querySelector(".logo");
-
-logo.addEventListener("mouseenter", () => {
-  logo.style.setProperty("--part-animation", "blink-1 0.6s both");
-});
-
-logo.addEventListener("mouseleave", () => {
-  logo.style.setProperty("--part-animation", "none");
-});
