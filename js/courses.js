dbf2024eba4e4b81adf186f52c7e7e73c5063959
Aleksandr Frolov <fr.28.09.07@gmail.com>
2025-11-13 12:04:01 +0100

    fix close-button and add overlay

--- js/courses.js
+++ js/courses.js
@@ -1,6 +1,30 @@
-document.querySelectorAll('.view-details').forEach((button) => {
+document.querySelectorAll('.course1').forEach((button) => {
   button.addEventListener('click', (e) => {
     const card = e.target.closest('.coursesList');
     card.classList.toggle('FirstCardOpen');
+  });
+});
+
+document.querySelectorAll('.course2').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const card = e.target.closest('.coursesList');
+    card.classList.toggle('SecondCardOpen');
+  });
+});
+
+document.querySelectorAll('.course3').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const card = e.target.closest('.coursesList');
+    card.classList.toggle('ThirdCardOpen');
+  });
+});
+
+document.querySelectorAll('.closeBtn').forEach((button) => {
+  button.addEventListener('click', (e) => {
+    const card = e.target.closest('.FirstCardOpen, .SecondCardOpen, .ThirdCardOpen');
+    if (card) {
+      card.classList.remove('FirstCardOpen', 'SecondCardOpen', 'ThirdCardOpen');
+      card.classList.add('coursesList');
+    }
   });
 });
